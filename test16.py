db36d6fdeed66c03429144a6003997762d5e2826
zohrejavanbakht82 <140422190+zohrejavanbakht82@users.noreply.github.com>
2023-08-07 23:34:07 +0330

    Add files via upload

---
+++ test16.py
@@ -0,0 +1,12 @@
+def shatrangee(a, b) :
+    my_string = "* # "
+    my_string1 = "# * "
+    for i in range(b):
+        for j in range(a) :
+            if i % 2 == 0 :
+                print(my_string[:a],end="")
+            elif i %2 != 0 :
+                print(my_string1[:a],end="")
+        print()
+        
+shatrangee(4,5)        
