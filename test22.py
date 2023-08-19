0f07783b6b66c43074d0cf858f4b07039ded2040
zohrejavanbakht82 <140422190+zohrejavanbakht82@users.noreply.github.com>
2023-08-19 10:47:40 +0330

    Add files via upload

---
+++ test22.py
@@ -0,0 +1,84 @@
+product = []
+def project():
+    f = open("test23.txt" , "r")
+    for L in f:
+        dic = {}
+        result = L.split(",")
+        dic = {"ID":result[0] ,"name" : result[1], "price" : result[2], "number" : result[3]}
+        print(dic)
+        product.append(dic)
+        print(product)
+def show_meno():
+    print("1 = add")
+    print("2 = delete")
+    print("3 = search")
+    print("4 = buy")
+    print("5 = edit")
+    print("6 = exit")
+    print("7 = show product")
+def add():
+    ID = input("Enter id product: ")
+    name = input("Enter name product: ")
+    price = input("Enter price producrt: ")
+    number = input("Enter nmber product: ")
+    dic = {"ID" : ID , "name" : name , "price" : price , "number": number}
+    product.append(dic)
+    print(product)
+def delete():
+    ID_DE = input("Enter id product: ")
+    f = open("test23.txt" , "r")
+    for L in f:
+        result = L.split(",")
+        if result[0] != ID_DE :
+            f.write("\n"+L)
+        else :
+            print("delete shod!!")
+    
+def search():
+    key = input("Enter the name product or ID product: ")
+    for i in product:
+        if key == i["ID"] or key == i["name"]:
+            print(i)
+            break
+    else :
+        print("not founded.")
+def buy():
+    pass
+def edit():
+    id = input("Enter ID product: ")
+    product.remove()
+    name = input("Enter a new name : ")
+    price = input("Enter the new price: ")
+    number = input("Enter the new number : ")
+    dic1= {"ID": id , "name": name , "price": price , "number": number}
+    product.append(dic1)
+    print("edit shod!!")
+    print(product)
+    
+    
+ 
+def exit():
+    pass
+def show_product():
+    print("ID \t name \t price \t number ")
+    for i in product:
+        print(i["ID"],"\t" , i["name"],"\t", i["price"],"\t" , i["number"])
+project()
+show_meno()
+user_choice = int(input("Enter a number 1 to 7:"))
+if user_choice == 1 :
+    add()
+elif user_choice == 2 :
+    delete()
+elif user_choice == 3 :
+    search()
+elif user_choice == 4 :
+    buy()
+elif user_choice == 5 :
+    edit()
+elif user_choice == 6 :
+    exit()
+elif user_choice == 7 :
+    show_product()
+else :
+    print("Error")
